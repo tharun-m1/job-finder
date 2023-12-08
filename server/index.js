8e6a8b743bb557713e240be4f119f8826c89aac2
tharunm1 <maddelatharun2@gmail.com>
2023-12-08 17:09:47 +0530

    boilerplate server setup

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    message: "All good!",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+      console.log("server running at http://localhost:4000");
+    })
+    .catch((error) => console.log(error));
+});
